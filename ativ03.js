f73dd8a09520ad539ce0f882c532ea384a42805d
ian <jesperiansantos.2021@gmail.com>
2021-10-21 20:18:32 -0300

    enviando arquivos

---
+++ ativ03.js
@@ -0,0 +1,4 @@
+const minhaFuncao = () => {
+    const mudarTexto = document.getElementById('boas-vindas');
+    mudarTexto.innerHTML = 'Olá, Seja bem vindo ao mundo JS!';
+}
---
+++ ativ04.js
@@ -0,0 +1,4 @@
+const minhaFuncao = () => {
+    const mudarTexto = document.getElementsByTagName('p');
+    mudarTexto.innerHTML = 'Olá, Seja bem vindo ao mundo JS!';
+}
